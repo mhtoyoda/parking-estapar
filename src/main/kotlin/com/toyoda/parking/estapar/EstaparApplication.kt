c7820fc2142fee084235e59758e74a086278dd8a
Marcelo Toyoda <marcelo.toyoda@maithatechcorp.onmicrosoft.com>
2026-06-23 11:38:21 -0300

    Init structure

---
+++ src/main/kotlin/com/toyoda/parking/estapar/EstaparApplication.kt
@@ -0,0 +1,11 @@
+package com.toyoda.parking.estapar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstaparApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstaparApplication>(*args)
+}
---
+++ src/test/kotlin/com/toyoda/parking/estapar/EstaparApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toyoda.parking.estapar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EstaparApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
