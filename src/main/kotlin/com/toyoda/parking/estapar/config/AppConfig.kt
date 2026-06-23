35520908658ddb52757709d2556eeeb134cbc8eb
Marcelo Toyoda <marcelo.toyoda@maithatechcorp.onmicrosoft.com>
2026-06-23 18:32:34 -0300

    Adjusts configs

--- src/main/kotlin/com/toyoda/parking/estapar/config/AppConfig.kt
+++ src/main/kotlin/com/toyoda/parking/estapar/config/AppConfig.kt
@@ -1,5 +1,9 @@
 package com.toyoda.parking.estapar.config
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -9,5 +13,12 @@ class AppConfig {
 
     @Bean
     fun restTemplate(): RestTemplate = RestTemplate()
+
+    @Bean
+    fun objectMapper(): ObjectMapper = ObjectMapper().apply {
+        registerModule(JavaTimeModule())
+        disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+    }
 
 }
